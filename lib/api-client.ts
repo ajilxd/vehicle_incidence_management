897d9f49afde9f36d1f6c8667f6de46d346e739b
Ajil pramod <ajilpramodone@gmail.com>
2025-08-27 20:13:04 +0530

    Add  apiClient for API interactions

---
+++ lib/api-client.ts
@@ -0,0 +1,16 @@
+import axios, { AxiosInstance } from "axios";
+
+export const apiClient: AxiosInstance = axios.create({
+  baseURL: "/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+apiClient.interceptors.response.use(
+  (response) => response.data,
+  (error) => {
+    console.error("API Error:", error.response?.data || error.message);
+    return Promise.reject(error);
+  }
+);
