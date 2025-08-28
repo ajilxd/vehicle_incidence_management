47d43489c4d51313a5388e320ead8e06662517d1
Ajil pramod <ajilpramodone@gmail.com>
2025-08-28 12:07:02 +0530

    Add handleApi in users route

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,10 +1,13 @@
+import { handleApi } from "@/lib/handleApi";
 import prisma from "@/lib/prisma";
 import { UserResponse } from "@/schemas/response/user";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(request: NextRequest) {
+const getUsers = async (request: NextRequest) => {
   const users: UserResponse = await prisma.user.findMany({
     select: { id: true, name: true, email: true, role: true },
   });
   return NextResponse.json(users);
-}
+};
+
+export const GET = handleApi(getUsers);
