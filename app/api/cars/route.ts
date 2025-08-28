3d78b14b86400f18548df633a50c8db83deefae3
Ajil pramod <ajilpramodone@gmail.com>
2025-08-28 12:05:33 +0530

    Add handleApi in cars route

--- app/api/cars/route.ts
+++ app/api/cars/route.ts
@@ -1,10 +1,13 @@
 import prisma from "@/lib/prisma";
 import { carResponse } from "@/schemas/response/car";
 import { NextRequest, NextResponse } from "next/server";
+import { handleApi } from "@/lib/handleApi";
 
-export async function GET(request: NextRequest) {
+const getCars = async (request: NextRequest) => {
   const cars: carResponse = await prisma.car.findMany({
     select: { id: true, make: true, model: true, licensePlate: true },
   });
   return NextResponse.json(cars);
-}
+};
+
+export const GET = handleApi(getCars);
